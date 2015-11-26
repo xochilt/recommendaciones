50cc4df3800f6451fdf9aea09a636c7f81571ac2
xochilt <xochilt.ramirez@gmail.com>
2015-11-26 15:38:10 -0800

    Update matrixTest.py

--- matrixTest.py
+++ matrixTest.py
@@ -1,34 +1,32 @@
 import numpy 
+import math
+from math import *
+import decimal
 
-def mat():
+
+#Funcion para trabajar con Movielens 100m
+def matrizfac():
     #Insertar los ratings.
-    music={}
     movies={}
-    #for line in open('movies.dat'):
-       #(id,title)=line.split('::')[0:2]
     for line in open('u.item'):
-     (id,title)=line.split('|')[0:2]
-     movies[id]=id
+         (id,title)=line.split('|')[0:2]
+         movies[id]=id
     # Load data
-    #prefs={}
     items=[]
     users=[]
     listarat=[]
-    #for line in open('ml-1m//r1.base'):
     for line in open('u3.base'):
-     tem=[]
-     (user,movieid,rating,ts)=line.split('\t')
-     #prefs.setdefault(user,{})
-     #prefs[user][movies[movieid]]=float(rating)
-     tem.append(user)
-     tem.append(movieid)
-     tem.append(rating)
-     listarat.append(tem)
-     if movieid in items:continue
-     items.append(movieid)
-     if user in users:continue
-     users.append(user)
-    cols=943
+         tem=[]
+         (user,movieid,rating,ts)=line.split('\t')
+         tem.append(user)
+         tem.append(movieid)
+         tem.append(rating)
+         listarat.append(tem)
+         if movieid in items:continue
+         items.append(movieid)
+         if user in users:continue
+         users.append(user)
+    cols=943#Size Movielens.
     rows=1682
     m=[] 
     matriz = numpy.ones((rows, cols)) * 0
@@ -38,36 +36,62 @@ def mat():
     c=0
     for i in listarat:
         matriz[ int(i[1]) -1 ][ int(i[0]) -1 ] = int(i[2])
-        #print  'pos1', 'pos2', int(i[1]) -1 , int(i[0]) -1 , 'rating:', float(i[2])
         c=c+1             
     print 'c', c
-    print 'trabajando con u3.base...'
+    #Save matrix.
     numpy.savetxt('/Users/xochilt/mf/matrices2/movielens-u3base.txt',matriz,fmt='%1.2f', delimiter=',')
-    return matriz,items,users, listarat
+    #Regresa la matriz y la lista de ratings de la matriz.
+    return matriz,listarat
 
-
-
+#Funcion para crear matrices iniciales para movielens-1m y 10m.
 def mat1m():
     #Insertar los ratings.
-    # music={}
-    # movies={}
-    # for line in open('movies.dat'):
-    #  #print line
-    #  (id,title)=line.split('::')[0:2]
-    #  movies[id]=id
-    # # Load data
-    #prefs={}
+    movies={}
+    for line in open('movies.dat'):
+         (id,title)=line.split('::')[0:2]
+         movies[id]=id
+    # Load data
+    items=[]
+    users=[]
+    listarat=[]
+    for line in open('ml-1m//train3.txt'):
+         tem=[]
+         (user,movieid,rating)=line.split()
+         tem.append(user)
+         tem.append(movieid)
+         tem.append(rating)
+         listarat.append(tem)
+         if movieid in items:continue
+         items.append(movieid)
+         if user in users:continue
+         users.append(user)
+    cols=6040#Size dataset.
+    rows=3952
+    m=[] 
+    #Crea la matriz inicial de ceros.
+    matriz = numpy.ones((rows, cols)) * 0
+    c=0
+    for i in listarat:
+        matriz[ int(i[1]) -1 ][ int(i[0]) -1 ] = int(i[2])
+        c=c+1             
+    
+    print 'Total de ratings insertados: ', c
+    #Guardar la matriz.
+    numpy.savetxt('/Users/xochilt/mf/matrices2/movielens-r1base.txt',matriz,fmt='%1.2f', delimiter=',')
+    #Regresa la matriz y la lista de ratings de la matriz.
+    return matriz, listarat
+
+
+
+#Funcion para crear las listas de Filmtrust.
+#No crea la matriz para factorizar.
+def filmtrust():
     items=[]
     users=[]
     listarat=[]
     for line in open('filmtrust//ratings-all.txt'):
      tem=[]
-     #print line
-
      (user,movieid,rating)=line.split('\t')
-     #(user,movieid,rating,ts)=line.split('::')   #\t')
-     #prefs.setdefault(user,{})
-     #prefs[user][movies[movieid]]=float(rating)
      tem.append(user)
      tem.append(movieid)
      tem.append(float(rating))
@@ -76,26 +100,12 @@ def mat1m():
      items.append(movieid)
      if user in users:continue
      users.append(user)
-    # cols=71567#6040
-    # rows=65133#10681#3952
-    # m=[] 
-    # matriz = numpy.ones((rows, cols)) * 0
-    # print 'items',len(matriz)
-    # print 'users',len(matriz[0])
-    # c=0
-    # for i in listarat:
-    #     print i
-    #     matriz[ int(i[1]) -1 ][ int(i[0]) -1 ] = int(i[2])
-    #     #print  'pos1', 'pos2', int(i[1]) -1 , int(i[0]) -1 , 'rating:', float(i[2])
-    #     c=c+1             
-    # print 'c', c
-    print 'ratings..10m...'
-    #numpy.savetxt('/Users/xochilt/mf/matrices2/movielens-10m.txt',matriz,fmt='%1.2f', delimiter=',')
-    return listarat, users,items #matriz,items,users, listarat
+     
+    print 'ratings...'
+    numpy.savetxt('/Users/xochilt/mf/matrices2/ratings.txt',matriz,fmt='%1.2f', delimiter=',')
+    return listarat, users,items
 
-
-
-
+#Funcion para hacer datasets de ml-1m y ml-10m, datasets train y test.
 def moviedts(ini,end):
     movies={}
     for line in open('movies.dat'):
@@ -118,7 +128,6 @@ def moviedts(ini,end):
         users.append(user)
 
     print 'listrain', len(listrain)
-
     print 'creando  datasets...'
     listest=[]
 
@@ -127,24 +136,17 @@ def moviedts(ini,end):
         listest.append(listrain[j])
     
     #eliminamos de la lista
-
-    print 'crear el train.' 
+    #crear el dataset de entrenamiento. 
     for k in listest:
         if k in listrain:
             listrain.remove(k)
-
-    print 'len listrain', len(listrain)
-    print 'len listest', len(listest)
-
+    #Devuelve el conjunto de datos de entrenamiento y de prueba.
     return listrain,listest
  
-
-
-
-
-import math
-from math import *
-import decimal
+ 
+#Funcion para obtener el error RMSE y  MAE.
+#Utiliza la matriz factorizada con los datos de prueba
+#y la matriz test, las compara y obtiene el error.
 def get_error(R,nR,users,items):
     sumarmse=0
     sumamae=0
@@ -154,12 +156,10 @@ def get_error(R,nR,users,items):
         for j in range(1,users):
             if R[i-1][j-1]==0.0:continue
             rmsed = ((R[i-1][j-1])-(nR[i-1][j-1]))**2
-            # print R[i-1][j-1], nR[i-1][j-1]
             # print 'rmse',rmsed
             
             maed= fabs((R[i-1][j-1])-(nR[i-1][j-1]))
             c=c+1.0
-            # print R[i-1][j-1],nR[i-1][j-1]
             # print 'maed', format(maed, '.5f')
             
             sumarmse=sumarmse+rmsed
@@ -167,66 +167,38 @@ def get_error(R,nR,users,items):
             
     t=(sumarmse/c)
     rmse = sqrt(t)
-
     mae=sumamae/c
-
-    print 'mae:', mae 
-    print 'rmse:', rmse
     print 'c',c
-
+    #Devuelve el error calculado.
     return rmse,mae  
 
-
+#funcion para salvar la matriz factorizada.
 def savedata(matrix):
-    import numpy
     numpy.savetxt('matrix.txt', matrix, delimiter=',', fmt='%1.3f')
     return
 
-
+#Funcion para crear las matrices iniciales para factorizar con
+#Music dataset.
 def matmusic(music):
     items=[]
+    #music es el dataset.
     for i in music:
         if i[1] in items:continue
         items.append(i[1])        
 
     items.sort()
-    cols=42
+    cols=42 #Size dataset.
     rows=len(items)
-
+    #Crea la matriz inicial de cero.
     mat = numpy.ones((rows, cols)) * 0
-
+    #Agregamos los ratings a la matriz inicial.
     for i in music:
         mat[i[1]-1][i[0]-1]=i[2]
-    
         
     #Creando el diccionario para utilizarlo en las recomendaciones.
     pref = dict([(t[0],{}) for t in music])
     for t in music:
         pref[t[0]][t[1]]=float(t[2])
-    
+    #Devuelve las preferencias de los usuarios, la matriz de ratings.
+    return pref,mat
 
-    return pref,mat,items
-
-
-def matmovie(movie):
-    items=[]
-    for i in movie:
-        if i[1]  in items:continue
-        items.append(i[1])        
-
-    items.sort()
-    cols=943
-    rows=1682
-
-    mat = numpy.ones((rows, cols)) * 0
-
-    for i in movie:
-        mat[i[1]-1][i[0]-1]=i[2]
-    
-        
-    #Creando el diccionario para utilizarlo en las recomendaciones.
-    pref = dict([(t[0],{}) for t in movie])
-    for t in movie:
-        pref[t[0]][t[1]]=float(t[2])
-                    
-    return pref,mat,items
