bd4e46950dde760f576c66a5a499b3226372de81
xochilt <xochilt.ramirez@gmail.com>
2015-11-26 15:11:07 -0800

    Update factorizaMatriz.py

--- factorizaMatriz.py
+++ factorizaMatriz.py
@@ -1,1 +1,51 @@
+#!/usr/bin/python
+#
+# Created by Albert Au Yeung (2010)
+#
+# An implementation of matrix factorization
+#
+try:
+    import numpy
+except:
+    print "This implementation requires the numpy module."
+    exit(0)
 
+###############################################################################
+
+"""
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    P     : an initial matrix of dimension N x K
+    Q     : an initial matrix of dimension M x K
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices P and Q
+    C:\Python27\Scripts>pip2.7.exe install "C:\Downloads\pygame-1.9.2a0-cp27-none-win_amd64.whl"
+    steps=5000
+""" 
+import time
+
+def matrix_factorization(R, P, Q, K, steps=50, alpha=0.00002, beta=0.002):
+    Q = Q.T
+    for step in xrange(steps):
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                    for k in xrange(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = numpy.dot(P,Q)
+        e = 0
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+                    for k in xrange(K):
+                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+        if e < 0.001:
+            break
+    return P, Q.T
